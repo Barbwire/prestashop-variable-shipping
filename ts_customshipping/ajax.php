8f836b12614de87f0fb4fdab3d1def71150c1016
Barbara Leth <info@tecserve.eu>
2018-11-16 15:56:42 +0100

    code standard violations fixed

--- ts_customshipping/ajax.php
+++ ts_customshipping/ajax.php
@@ -1,12 +1,22 @@
 <?php
-include_once('../../config/config.inc.php');
-include_once('../../init.php');
-include_once('../../modules/ts_customshipping/ts_customshipping.php');
 
-if (!Tools::getValue('ajax') || Tools::getValue('token') != sha1(_COOKIE_KEY_ . 'ts_customshipping')) {
+/**
+ * This file is part of PHP CS Fixer.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *     Dariusz Rumiński <dariusz.ruminski@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+include_once '../../config/config.inc.php';
+include_once '../../init.php';
+include_once '../../modules/ts_customshipping/ts_customshipping.php';
+
+if (!Tools::getValue('ajax') || Tools::getValue('token') !== sha1(_COOKIE_KEY_.'ts_customshipping')) {
     die;
 }
 
 // Set shipping for this customer and cart
-Configuration::updateValue('TS_CUSTOM_SHIPPING_CARRIER_VALUE', round(floatval(Tools::getValue('value')), 2));
-?>
+Configuration::updateValue('TS_CUSTOM_SHIPPING_CARRIER_VALUE', round((float) Tools::getValue('value'), 2));
--- ts_customshipping/ts_customshipping.php
+++ ts_customshipping/ts_customshipping.php
@@ -1,37 +1,22 @@
 <?php
+
 /**
- * 2007-2018 PrestaShop
+ * This file is part of PHP CS Fixer.
  *
- * NOTICE OF LICENSE
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *     Dariusz Rumiński <dariusz.ruminski@gmail.com>
  *
- * This source file is subject to the Academic Free License (AFL 3.0)
- * that is bundled with this package in the file LICENSE.txt.
- * It is also available through the world-wide-web at this URL:
- * http://opensource.org/licenses/afl-3.0.php
- * If you did not receive a copy of the license and are unable to
- * obtain it through the world-wide-web, please send an email
- * to license@prestashop.com so we can send you a copy immediately.
- *
- * DISCLAIMER
- *
- * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
- * versions in the future. If you wish to customize PrestaShop for your
- * needs please refer to http://www.prestashop.com for more information.
- *
- * @author    PrestaShop SA <contact@prestashop.com>
- * @copyright 2007-2018 PrestaShop SA
- * @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
- *  International Registered Trademark & Property of PrestaShop SA
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
  */
 
 use PrestaShop\PrestaShop\Core\Addon\Module\ModuleManagerBuilder;
-
 
 if (!defined('_PS_VERSION_')) {
     exit;
 }
 
-class Ts_Customshipping extends CarrierModule
+class Ts_CustomShipping extends CarrierModule
 {
     public $id_carrier;
 
@@ -42,11 +27,9 @@ class Ts_Customshipping extends CarrierModule
         $this->version = '1.0.0';
         $this->author = 'Barbara Leth';
         $this->need_instance = 1;
-        $this->ps_versions_compliancy = array('min' => '1.7', 'max' => _PS_VERSION_);
+        $this->ps_versions_compliancy = ['min' => '1.7', 'max' => _PS_VERSION_];
 
-        /**
-         * Set $this->bootstrap to true if your module is compliant with bootstrap (PrestaShop 1.6)
-         */
+        // Set $this->bootstrap to true if your module is compliant with bootstrap (PrestaShop 1.6)
         $this->bootstrap = true;
 
         parent::__construct();
@@ -59,52 +42,64 @@ class Ts_Customshipping extends CarrierModule
 
         if ($moduleManager->isInstalled($this->name)) {
             // Getting carrier list
-            global $cookie;
-            $carriers = Carrier::getCarriers($cookie->id_lang, true, false, false, NULL, PS_CARRIERS_AND_CARRIER_MODULES_NEED_RANGE);
+            $carriers = Carrier::getCarriers(
+                $this->context->language->id,
+                true,
+                false,
+                false,
+                null,
+                PS_CARRIERS_AND_CARRIER_MODULES_NEED_RANGE
+            );
 
             // Saving id carrier list
-            $id_carrier_list = array();
+            $id_carrier_list = [];
             foreach ($carriers as $carrier) {
                 $id_carrier_list[] .= $carrier['id_carrier'];
             }
 
             // Testing if Carrier ID exists
-            $warning = array();
-            if (!in_array((int)(Configuration::get('TS_CUSTOM_SHIPPING_CARRIER_ID')), $id_carrier_list)) {
-                $warning[] .= $this->l('TS Custom Shipping Carrier') . ' ';
+            $warning = [];
+            if (!in_array((int) (Configuration::get('TS_CUSTOM_SHIPPING_CARRIER_ID')), $id_carrier_list, true)) {
+                $warning[] .= $this->l('TS Custom Shipping Carrier').' ';
             }
             if (count($warning)) {
-                $this->warning .= implode(' , ', $warning) . $this->l('must be configured to use this module correctly') . ' ';
+                $this->warning .= implode(
+                    ' , ', $warning
+                ).$this->l('must be configured to use this module correctly').' ';
             }
         }
     }
 
     /**
      * Don't forget to create update methods if needed:
-     * http://doc.prestashop.com/display/PS16/Enabling+the+Auto-Update
+     * http://doc.prestashop.com/display/PS16/Enabling+the+Auto-Update.
      */
     public function install()
     {
-        $carrierConfig = array(
-            0 => array('name' => 'Custom Shipping',
+        $carrierConfig = [
+            0 => [
+                'name' => 'Custom Shipping',
                 'active' => true,
                 'deleted' => 0,
                 'id_tax_rule_group' => '2',
                 'shipping_handling' => false,
                 'range_behavior' => 0,
-                'delay' => array('fr' => 'Custom', 'en' => 'Custom', Language::getIsoById(Configuration::get('PS_LANG_DEFAULT')) => 'Custom'),
+                'delay' => [
+                    'fr' => 'Custom',
+                    'en' => 'Custom',
+                    Language::getIsoById(Configuration::get('PS_LANG_DEFAULT')) => 'Custom',
+                ],
                 'is_module' => true,
                 'shipping_external' => true,
                 'external_module_name' => 'ts_customshipping',
-                'need_range' => true
-            ),
-        );
-
+                'need_range' => true,
+            ],
+        ];
 
         $id_carrier1 = $this->installExternalCarrier($carrierConfig[0]);
-        $Carrier = new Carrier((int)$id_carrier1);
-        $Carrier->setTaxRulesGroup((int)$this->getTaxRulesGroupMostUsed());
-        Configuration::updateValue('TS_CUSTOM_SHIPPING_CARRIER_ID',  (int)$id_carrier1);
+        $Carrier = new Carrier((int) $id_carrier1);
+        $Carrier->setTaxRulesGroup((int) $this->getTaxRulesGroupMostUsed());
+        Configuration::updateValue('TS_CUSTOM_SHIPPING_CARRIER_ID', (int) $id_carrier1);
 
         return parent::install() && $this->registerHook('displayBackOfficeHeader');
     }
@@ -112,19 +107,28 @@ class Ts_Customshipping extends CarrierModule
     public function uninstall()
     {
         // Delete External Carrier
-        $Carrier1 = new Carrier((int)(Configuration::get('TS_CUSTOM_SHIPPING_CARRIER_ID')));
+        $Carrier1 = new Carrier((int) (Configuration::get('TS_CUSTOM_SHIPPING_CARRIER_ID')));
         // If external carrier is default set other one as default
-        if (Configuration::get('PS_CARRIER_DEFAULT') == (int)($Carrier1->id)) {
-            global $cookie;
-            $carriersD = Carrier::getCarriers($cookie->id_lang, true, false, false, NULL, PS_CARRIERS_AND_CARRIER_MODULES_NEED_RANGE);
-            foreach ($carriersD as $carrierD)
-                if ($carrierD['active'] AND !$carrierD['deleted'] AND ($carrierD['name'] != $this->name))
+        if (Configuration::get('PS_CARRIER_DEFAULT') === (int) ($Carrier1->id)) {
+            $carriersD = Carrier::getCarriers(
+                $this->context->language->id,
+                true,
+                false,
+                false,
+                null,
+                PS_CARRIERS_AND_CARRIER_MODULES_NEED_RANGE
+            );
+            foreach ($carriersD as $carrierD) {
+                if ($carrierD['active'] && !$carrierD['deleted'] && ($carrierD['name'] !== $this->name)) {
                     Configuration::updateValue('PS_CARRIER_DEFAULT', $carrierD['id_carrier']);
+                }
+            }
         }
 
         // Then delete Carrier
-        if (!$Carrier1->delete())
+        if (!$Carrier1->delete()) {
             return false;
+        }
         Configuration::deleteByName('TS_CUSTOM_SHIPPING_CARRIER_ID');
         Configuration::deleteByName('TS_CUSTOM_SHIPPING_CARRIER_VALUE');
 
@@ -132,19 +136,17 @@ class Ts_Customshipping extends CarrierModule
         return parent::uninstall() && $this->unregisterHook('displayBackOfficeHeader');
     }
 
-
     /**
      * Add the CSS & JavaScript files you want to be loaded in the BO.
      */
     public function hookDisplayBackOfficeHeader()
     {
+        $this->context->controller->addJS($this->_path.'views/js/ts_customshipping.js');
+        $this->context->controller->addCSS($this->_path.'views/css/ts_customshipping.css');
 
-        $this->context->controller->addJS($this->_path . 'views/js/ts_customshipping.js');
-        $this->context->controller->addCSS($this->_path . 'views/css/ts_customshipping.css');
-
-        $out =  '<script>var customshipping_carrier_id = ' . Configuration::get('TS_CUSTOM_SHIPPING_CARRIER_ID') . ';</script>';
-        $out .= '<script>var customshipping_token = "' . sha1(_COOKIE_KEY_ . 'ts_customshipping') . '";</script>';
-        $out .= '<script>var customshipping_ajax_url = "' . $this->_path . 'ajax.php' . '";</script>';
+        $out = '<script>var customshipping_carrier_id = '.Configuration::get('TS_CUSTOM_SHIPPING_CARRIER_ID').';</script>';
+        $out .= '<script>var customshipping_token = "'.sha1(_COOKIE_KEY_.'ts_customshipping').'";</script>';
+        $out .= '<script>var customshipping_ajax_url = "'.$this->_path.'ajax.php'.'";</script>';
 
         return $out;
     }
@@ -157,15 +159,17 @@ class Ts_Customshipping extends CarrierModule
     public function getOrderShippingCostExternal($params)
     {
         $context = Context::getContext();
-        if (!$context->employee || !$context->employee->id)
+        if (!$context->employee || !$context->employee->id) {
             return false;
+        }
 
         $value = Configuration::get('TS_CUSTOM_SHIPPING_CARRIER_VALUE');
-        return $value ? round(floatval($value), 2) : 0.00;
+
+        return $value ? round((float) $value, 2) : 0.00;
     }
 
     /**
-     * @return false|null|string ID Tax rule group most used for existing active carriers
+     * @return null|false|string ID Tax rule group most used for existing active carriers
      */
     public static function getTaxRulesGroupMostUsed()
     {
@@ -173,22 +177,23 @@ class Ts_Customshipping extends CarrierModule
                     SELECT id_tax_rules_group 
                     FROM (
                         SELECT COUNT(*) n, ctrg.id_tax_rules_group 
-                        FROM ' . _DB_PREFIX_ . 'carrier c 
-                        JOIN ' . _DB_PREFIX_ . 'carrier_tax_rules_group_shop ctrg ON (c.id_carrier = ctrg.id_carrier) 
-                        JOIN ' . _DB_PREFIX_ . 'tax_rules_group trg ON (trg.id_tax_rules_group = ctrg.id_tax_rules_group)
+                        FROM '._DB_PREFIX_.'carrier c 
+                        JOIN '._DB_PREFIX_.'carrier_tax_rules_group_shop ctrg ON (c.id_carrier = ctrg.id_carrier) 
+                        JOIN '._DB_PREFIX_.'tax_rules_group trg ON (trg.id_tax_rules_group = ctrg.id_tax_rules_group)
                         WHERE c.active = 1 AND c.deleted = 0 AND trg.active = 1 AND trg.deleted = 0
                         GROUP BY ctrg.id_tax_rules_group
                         ORDER BY n DESC
                         LIMIT 1
-                    ) most_used'
-        );
+                    ) most_used');
     }
 
     /**
      * @param $config
-     * @return bool|int
+     *
      * @throws PrestaShopDatabaseException
      * @throws PrestaShopException
+     *
+     * @return bool|int
      */
     public static function installExternalCarrier($config)
     {
@@ -206,25 +211,27 @@ class Ts_Customshipping extends CarrierModule
 
         $languages = Language::getLanguages(true);
         foreach ($languages as $language) {
-            if ($language['iso_code'] == 'fr')
-                $carrier->delay[(int)$language['id_lang']] = $config['delay'][$language['iso_code']];
-            if ($language['iso_code'] == 'en')
-                $carrier->delay[(int)$language['id_lang']] = $config['delay'][$language['iso_code']];
-            if ($language['iso_code'] == Language::getIsoById(Configuration::get('PS_LANG_DEFAULT')))
-                $carrier->delay[(int)$language['id_lang']] = $config['delay'][$language['iso_code']];
+            if ('fr' === $language['iso_code']) {
+                $carrier->delay[(int) $language['id_lang']] = $config['delay'][$language['iso_code']];
+            }
+            if ('en' === $language['iso_code']) {
+                $carrier->delay[(int) $language['id_lang']] = $config['delay'][$language['iso_code']];
+            }
+            if ($language['iso_code'] === Language::getIsoById(Configuration::get('PS_LANG_DEFAULT'))) {
+                $carrier->delay[(int) $language['id_lang']] = $config['delay'][$language['iso_code']];
+            }
         }
 
         if ($carrier->add()) {
             $groups = Group::getGroups(true);
-            $data = array();
+            $data = [];
             foreach ($groups as $group) {
-                $data[] = array(
-                    'id_carrier' => (int)($carrier->id),
-                    'id_group' => (int)($group['id_group']),
-                );
+                $data[] = [
+                    'id_carrier' => (int) ($carrier->id),
+                    'id_group' => (int) ($group['id_group']),
+                ];
             }
             Db::getInstance()->insert('carrier_group', $data, false, false, Db::INSERT);
-
 
             $rangePrice = new RangePrice();
             $rangePrice->id_carrier = $carrier->id;
@@ -240,20 +247,41 @@ class Ts_Customshipping extends CarrierModule
 
             $zones = Zone::getZones(true);
             foreach ($zones as $zone) {
-                Db::getInstance()->insert('carrier_zone', array('id_carrier' => (int)($carrier->id), 'id_zone' => (int)($zone['id_zone'])), false, false, Db::INSERT);
-                Db::getInstance()->insert('delivery', array('id_carrier' => (int)($carrier->id), 'id_range_price' => (int)($rangePrice->id), 'id_range_weight' => NULL, 'id_zone' => (int)($zone['id_zone']), 'price' => '0'), false, false, Db::INSERT);
-                Db::getInstance()->insert('delivery', array('id_carrier' => (int)($carrier->id), 'id_range_price' => NULL, 'id_range_weight' => (int)($rangeWeight->id), 'id_zone' => (int)($zone['id_zone']), 'price' => '0'), false, false, Db::INSERT);
+                Db::getInstance()->insert(
+                    'carrier_zone',
+                    ['id_carrier' => (int) ($carrier->id), 'id_zone' => (int) ($zone['id_zone'])],
+                    false,
+                    false,
+                    Db::INSERT
+                );
+                Db::getInstance()->insert('delivery', [
+                    'id_carrier' => (int) ($carrier->id),
+                    'id_range_price' => (int) ($rangePrice->id),
+                    'id_range_weight' => null,
+                    'id_zone' => (int) ($zone['id_zone']),
+                    'price' => '0',
+                ], false, false, Db::INSERT);
+                Db::getInstance()->insert('delivery', [
+                    'id_carrier' => (int) ($carrier->id),
+                    'id_range_price' => null,
+                    'id_range_weight' => (int) ($rangeWeight->id),
+                    'id_zone' => (int) ($zone['id_zone']),
+                    'price' => '0',
+                ], false, false, Db::INSERT);
             }
 
             // Copy Logo
-            if (!copy(dirname(__FILE__) . '/carrier.jpg', _PS_SHIP_IMG_DIR_ . '/' . (int)$carrier->id . '.jpg'))
+            if (!copy(
+                dirname(__FILE__).'/views/img/carrier.jpg',
+                _PS_SHIP_IMG_DIR_.'/'.(int) $carrier->id.'.jpg'
+            )) {
                 return false;
+            }
 
             // Return ID Carrier
-              return (int)($carrier->id);
+            return (int) ($carrier->id);
         }
 
         return false;
     }
-
 }
